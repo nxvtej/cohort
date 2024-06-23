2b1a3b1e2051ebec332212c70a609bb999a0181c
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-06-23 22:00:44 +0530

    end points done

--- week5/todoApplication/backend/index.js
+++ week5/todoApplication/backend/index.js
@@ -1,22 +1,45 @@
 const { express } = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 const app = express();
 
 app.use(express.json());
-app.post("/todo", function (req, res) {
+app.post("/todo", async function (req, res) {
 	const creatPayload = req.body;
-	const parsedPAyload = createTodo.safeParse(creatPayload);
-	if (!parsedPAyload.success) {
+	const parsedPayload = createTodo.safeParse(creatPayload);
+	if (!parsedPayload.success) {
 		res.status(411).json({
 			msg: "you sent wrong inputs.",
 		});
 		return;
 	}
+
+	try {
+		await todo.create({
+			title: parsedPayload.title,
+			description: creatPayload.description,
+			completed: false,
+		});
+
+		res.json({
+			msg: "Todo created successfully.",
+		});
+	} catch (e) {
+		res.status(404).json({
+			msg: "db failure",
+		});
+	}
 });
 
-app.get("/todos", function (req, res) {});
+app.get("/todos", async function (req, res) {
+	const todos = await todo.find();
+	console.log(todos);
+	res.json({
+		todos,
+	});
+});
 
-app.put("/completed", function (req, res) {
+app.put("/completed", async function (req, res) {
 	const updateTodo = req.body;
 	const parsedUpdate = updateTodo.safeParse(updateTodo);
 
@@ -26,4 +49,17 @@ app.put("/completed", function (req, res) {
 		});
 		return;
 	}
+
+	await todo.update(
+		{
+			_id: parsedUpdate.id,
+		},
+		{
+			completed: true,
+		}
+	);
+
+	res.json({
+		msg: "todo marked completed",
+	});
 });
