a57d30ee4e64e5d2d358e55cff54e57d2ed1f8eb
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-06-28 16:01:15 +0530

    starting with hooks

---
+++ week6/firsts/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<Header title='navdeep'></Header>
+			<Header title='singh'></Header>
+		</>
+	);
+}
+
+function Header({ title }) {
+	return <div>{title}</div>;
+}
+
+export default App;
+``;
